403d8486403d2113b5e03219f0a78533131b704c
Jaehwan <dialektike@gmail.com>
2022-04-23 21:16:34 +0900

    lib 세팅 및 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    tossi_for_rust::hello();
+
+    let mut sentence = String::new();
+
+    io::stdin()
+        .read_line(&mut sentence)
+        .expect("줄을 읽는데 실패했습니다.");
+
+    let raw = sentence.trim();
+
+    println!("콘솔에서 입력한 값: {}", raw.to_string());
 }
