b9413a53032a9eef6aab24ae90adc9755daaf814
Jaehwan <dialektike@gmail.com>
2022-05-13 19:35:17 +0900

    cargo fmt 을 이용해 코드 formatting 수정

--- src/hangeul.rs
+++ src/hangeul.rs
@@ -1,7 +1,16 @@
 // 초성, 중성, 종성 배열 정의
-static INITIAL: [char; 19] = ['ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'];    
-static MEDIAL: [char; 21] = ['ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ'];
-static FINAL: [char; 28] = [' ','ㄱ','ㄲ','ㄳ','ㄴ','ㄵ','ㄶ','ㄷ','ㄹ','ㄺ','ㄻ','ㄼ','ㄽ','ㄾ','ㄿ','ㅀ','ㅁ','ㅂ','ㅄ','ㅅ','ㅆ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'];
+static INITIAL: [char; 19] = [
+    'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ',
+    'ㅌ', 'ㅍ', 'ㅎ',
+];
+static MEDIAL: [char; 21] = [
+    'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ',
+    'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ',
+];
+static FINAL: [char; 28] = [
+    ' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ',
+    'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ',
+];
 
 // 한글인지 체크하는 함수
 fn is_hangeul(word: char) -> bool {
@@ -23,17 +32,17 @@ pub fn join_phonemes(word: [char; 3]) -> char {
     let initial = '가' as u32;
     let offset = ((idx_begin * MEDIAL.len() + idx_middle) * FINAL.len() + idx_end) as u32;
     let output = char::from_u32(initial + offset).unwrap();
-    return output
+    return output;
 }
 
 // 한글자를 초,중,종성으로 구분하는 함수
-pub fn split_phonemes(word: char) -> [char; 3] { 
+pub fn split_phonemes(word: char) -> [char; 3] {
     // 조,중,종성을 담을 배열 정의
-    let mut phonemes: [char; 3] = [' '; 3]; 
+    let mut phonemes: [char; 3] = [' '; 3];
     // 받은 문자가 한글인지 확인, 한글이 아닐 경우 공백으로 출력
     if !is_hangeul(word) {
         println!("The word is not hangeul");
-        return phonemes
+        return phonemes;
     }
     //'가'와의 차이값 계산
     let unicode = word as u32;
@@ -41,16 +50,16 @@ pub fn split_phonemes(word: char) -> [char; 3] {
     let offset = unicode - initial;
     //초,중,종성 값 계산
     //초성
-    let idx_begin: usize = (offset/(21*28)) as usize;
+    let idx_begin: usize = (offset / (21 * 28)) as usize;
     phonemes[0] = INITIAL[idx_begin];
     //중성
-    let idx_middle: usize = ((offset/28)%21) as usize;
+    let idx_middle: usize = ((offset / 28) % 21) as usize;
     phonemes[1] = MEDIAL[idx_middle];
     //종성은 있는 경우에만 계산
-    if (((unicode-0xAC00)%(21*28))%28) != 0 {
-        let idx_end: usize = (offset%28) as usize;
+    if (((unicode - 0xAC00) % (21 * 28)) % 28) != 0 {
+        let idx_end: usize = (offset % 28) as usize;
         phonemes[2] = FINAL[idx_end];
     }
     //초,중,종성이 배열로 묶여서 전달
-    return phonemes
+    return phonemes;
 }
