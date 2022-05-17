677d269ee5201424ec52e46ed7dd5ac820596e64
Hyeonji-Ryu <cjstk3940@naver.com>
2022-05-17 14:17:55 +0900

    마지막 글자 찾는 로직 추가

--- src/filter.rs
+++ src/filter.rs
@@ -1,2 +1,32 @@
+use crate::hangeul::is_hangeul;
+
 static DIGITS: [char; 10] = ['영','일','이','삼','사','오','육','칠','팔','구'];
 
+//단어에서 마지막 글자를 찾아주는 함수
+pub fn find_last_letter(word: &str) -> char {
+    let filtered = filter_only_significant(word);
+    return filtered[filtered.len()-1];
+}
+
+//단어에서 불필요한 요소 제거하는 함수
+fn filter_only_significant(word: &str) -> Vec<char> {
+    let mut output: Vec<char> = Vec::new();
+    let mut bracket: bool = false;
+    for c in word.chars() {
+        //괄호 있는지 확인
+        if c == '(' {
+            bracket = true;
+        }
+        else if c == ')' {
+            bracket = false;
+        }
+        //한글이 아니라면 제거
+        if bracket {
+            continue;
+        }
+        else if is_hangeul(c) {
+            output.push(c);
+        }
+    }
+    return output;
+}
