89f7b5a016a7fb29c644d7e9217d078f2f4696bc
Jaehwan <dialektike@gmail.com>
2022-05-21 16:36:06 +0900

    lib 파일 정리

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+mod hangeul;
+
 pub fn hello() {
     println!("Hello, world!");
+}
+
+pub fn join_phonemes(word: [char; 3]) -> char {
+    hangeul::join_phonemes(word)
+}
+
+pub fn split_phonemes(word: char) -> [char; 3] {
+    hangeul::split_phonemes(word)
 }
