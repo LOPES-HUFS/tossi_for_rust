b6fb96dead8d9fff18fd5c3717b9bb202ff2b935
Hyeonji-Ryu <cjstk3940@naver.com>
2022-06-04 18:36:16 +0900

    main.rs 정리

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-use std::io;
-
 use library::postfix;
-mod filter;
-mod hangeul;
 
 fn main() {
-    println!("단어를 입력해주세요.");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("줄을 읽는데 실패했습니다.");
-
-    let word = input.trim();
-
     // 테스트
     println!("{:?}", postfix("   ", "은"));
     println!("{:?}", postfix("나뭇가지(만렙)", "이다"));
