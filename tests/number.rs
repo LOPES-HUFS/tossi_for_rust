233ba48c7463961bf81fe47e4f6f358a4e4ea5cc
Jae-Hwan <dialektike@gmail.com>
2022-06-25 14:38:35 +0900

    테스트할 내용 추가

--- tests/number.rs
+++ tests/number.rs
@@ -36,28 +36,43 @@ fn _change_int_char() {
 
 #[test]
 fn _change_num_to_hangeul() {
+    let test = "0";
+    let result = "영";
+    assert_eq!(result, change_num_to_hangeul(test));
+
+    let test = "1";
+    let result = "일";
+    assert_eq!(result, change_num_to_hangeul(test));
+
     let test = "2";
     let result = "이";
     assert_eq!(result, change_num_to_hangeul(test));
-
-    let test = "100009";
-    let result = "십만구";
-    assert_eq!(result, change_num_to_hangeul(test));
-
-    let test = "10000000000000000000000000000000000000000000000000";
-    let result = "십극";
-    assert_eq!(result, change_num_to_hangeul(test));
-
-    let test = "10010";
-    let result = "만십";
+    
+    let test = "10000";
+    let result = "만";
     assert_eq!(result, change_num_to_hangeul(test));
 
     let test = "10001";
     let result = "만일";
     assert_eq!(result, change_num_to_hangeul(test));
+    
+    let test = "10010";
+    let result = "만십";
+    assert_eq!(result, change_num_to_hangeul(test));
 
     let test = "100009";
     let result = "십만구";
     assert_eq!(result, change_num_to_hangeul(test));
-}
+    
+    let test = "12345";
+    let result = "만이천삼백사십오";
+    assert_eq!(result, change_num_to_hangeul(test));
 
+    let test = "100000000";
+    let result = "일억";
+    assert_eq!(result, change_num_to_hangeul(test));
+
+    let test = "10000000000000000000000000000000000000000000000000";
+    let result = "십극";
+    assert_eq!(result, change_num_to_hangeul(test));
+}
